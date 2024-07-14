c0e3cdec72eb5cfefea97a6786ab7ad1d40344c0
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-14 15:44:28 +0200

    add: created homepage

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,61 @@
+import HomeSection from "@/components/home-section";
+
+const sections = [
+ {
+  header: "Zwiększanie popularności",
+  description:
+   "Specjalizujemy się w budowaniu zasięgów na platformach społecznościowych, co pozwala dotrzeć do szerokiej grupy odbiorców."
+ },
+ {
+  header: "Nowatorskie rozwiązania",
+  description:
+   "Każda usługa jest indywidualnie dostosowywana do potrzeb klienta, co zwiększa skuteczność działań promocyjnych."
+ },
+ {
+  header: "Dostosowane usługi",
+  description:
+   "Każda usługa jest indywidualnie dostosowywana do potrzeb klienta, co zwiększa skuteczność działań promocyjnych."
+ },
+ {
+  header: "Doświadczenie i profesjonalizm",
+  description:
+   "Nasz zespół z wieloletnim doświadczeniem zapewnia wysoką jakość usług."
+ },
+ {
+  header: "Budowanie wizerunku",
+  description:
+   "Pomagamy w kreowaniu i zarządzaniu wizerunkiem marki, co zwiększa zaufanie i wartość na rynku."
+ },
+ {
+  header: "Indywidualne podejście",
+  description:
+   "Traktujemy każdego klienta indywidualnie, dostosowując strategie do jego celów."
+ },
+ {
+  header: "Efektywne zarządzanie budżetem",
+  description:
+   "Nasze doświadczenie pozwala na maksymalizację zwrotu z inwestycji przy efektywnym zarządzaniu budżetami reklamowymi."
+ }
+];
+
 export default function Home() {
- return <div>Home</div>;
+ return (
+  <div className="w-full h-full flex flex-col items-center">
+   <div className="h-screen flex justify-center items-center w-1/2">
+    <img
+     alt="Media Bliss"
+     src="mb-logo-light-1.svg"
+    />
+   </div>
+   <div>
+    {sections.map((section, index) => (
+     <HomeSection
+      key={index}
+      header={section.header}
+      description={section.description}
+     />
+    ))}
+   </div>
+  </div>
+ );
 }
