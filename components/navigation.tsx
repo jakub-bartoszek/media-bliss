b9a42dbf1da48aa999462b9f77201e54fa1fff2b
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-14 15:43:54 +0200

    add: created simple navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,5 +1,19 @@
+import { FaShoppingBag } from "react-icons/fa";
+
 const Navigation = () => {
- return <nav className=" max-w-[1400px] w-full h-full">Nav</nav>;
+ return (
+  <nav className="max-w-[1400px] w-full h-full p-4 flex justify-between items-center">
+   <a className="h-full">
+    <img
+     className="w-full h-full"
+     src="mb-logo-light-3.svg"
+    />
+   </a>
+   <a>
+    <FaShoppingBag className="h-6 w-6" />
+   </a>
+  </nav>
+ );
 };
 
 export default Navigation;
