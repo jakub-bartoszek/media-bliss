402038c3eef2c5af67b3339c22a2209eee89902c
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-14 15:52:42 +0200

    change: changed method of rendering content in main

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,7 +11,11 @@ export default function RootLayout({
    <header className="h-14 flex justify-center bg-white fixed top-0 left-0 w-full shadow-[0_0_30px_#00000050] z-30">
     <Navigation />
    </header>
-   <main className="max-w-[1400px] ml-auto mr-auto">{children}</main>
+   <main className="max-w-[1400px] ml-auto mr-auto">
+    <div className="w-full min-h-screen h-full flex flex-col items-center">
+     {children}
+    </div>
+   </main>
    <Footer />
   </>
  );
