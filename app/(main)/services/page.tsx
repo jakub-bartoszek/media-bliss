fde2762a7844bf7c00e14f2e9dbcb9c579a4eda9
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-14 19:43:24 +0200

    add: created services page

--- app/(main)/services/page.tsx
+++ app/(main)/services/page.tsx
@@ -1,5 +1,277 @@
+import ServiceOthersTile from "@/components/service-others-tile";
+import ServicePackageTile from "@/components/service-package-tile";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa6";
+
+const services = {
+ instagram: {
+  packages: [
+   {
+    id: 1,
+    name: "Pakiet Starter",
+    list: [
+     "500 Polskich obserwacji",
+     "5000 Międzynarodowych obserwacji",
+     "4000 Polubień (rozłożone do 5 postów)",
+     "10 000 Wyświetleń dla rolki",
+     "Gwarancja 90 Dni"
+    ],
+    price: 1699
+   },
+   {
+    id: 2,
+    name: "Pakiet Medium",
+    list: [
+     "1000 Polskich obserwacji",
+     "10 000 Międzynarodowych obserwacji",
+     "8000 Polubień (rozłożone do 8 postów)",
+     "20 000 Wyświetleń do 2 rolek",
+     "Gwarancja 90 Dni"
+    ],
+    price: 1999
+   },
+   {
+    id: 3,
+    name: "Pakiet High",
+    list: [
+     "2000 Polskich obserwacji",
+     "20 000 Międzynarodowych obserwacji",
+     "15 000 Polubień (rozłożone do 10 postów)",
+     "40 000 Wyświetleń do 4 rolek",
+     "Gwarancja 90 Dni"
+    ],
+    price: 3499
+   },
+   {
+    id: 4,
+    name: "Pakiet Konto",
+    list: [
+     "Konto Instagram z ponad 15 000 obserwacji (90% polskich odbiorców)",
+     "2000 Polskich obserwacji",
+     "40 000 Międzynarodowych obserwacji",
+     "35 000 Polubień (rozłożone do 10 postów)",
+     "100 000 Wyświetleń do 10 rolek",
+     "Gwarancja 90 Dni"
+    ],
+    price: 7999
+   },
+   {
+    id: 5,
+    name: "Pakiet Pro",
+    list: [
+     "Konto Instagram z ponad 50 000 obserwacji (90% polskich odbiorców)",
+     "Weryfikacja tego konta na Instagramie",
+     "60 000 Międzynarodowych obserwacji",
+     "50 000 Polubień (rozłożone do 10 postów)",
+     "150 000 Wyświetleń do 10 rolek",
+     "Gwarancja 90 Dni"
+    ],
+    price: 14999
+   }
+  ],
+  others: [
+   {
+    id: 6,
+    name: "Weryfikacja konta",
+    image: "service-images/instagram/verify.png",
+    price: 5999
+   },
+   {
+    id: 7,
+    name: "1000 Polskich obserwacji",
+    image: "service-images/instagram/1000-follow-pl.png",
+    price: 349
+   },
+   {
+    id: 8,
+    name: "500 Polskich polubień",
+    image: "service-images/instagram/500-like-pl.png",
+    price: 149
+   },
+   {
+    id: 9,
+    name: "1000 Międzynarodowych obserwacji",
+    image: "service-images/instagram/1000-follow.png",
+    price: 149
+   },
+   {
+    id: 10,
+    name: "1000 Międzynarodowych polubień",
+    image: "service-images/instagram/1000-like.png",
+    price: 99
+   },
+   {
+    id: 11,
+    name: "10 000 Wyświetleń rolek",
+    image: "service-images/instagram/10000-reels.png",
+    price: 99
+   }
+  ]
+ },
+ tiktok: {
+  packages: [
+   {
+    id: 12,
+    name: "Pakiet Starter",
+    list: [
+     "500 Polskich obserwacji",
+     "4000 Międzynarodowych obserwacji",
+     "6000 Polubień (rozłożone do 5 TikToków)",
+     "20 000 Wyświetleń (rozłożone do 2 TikToków)",
+     "Gwarancja 90 Dni"
+    ],
+    price: 999
+   },
+   {
+    id: 13,
+    name: "Pakiet Medium",
+    list: [
+     "1000 Polskich obserwacji",
+     "8000 Międzynarodowych obserwacji",
+     "15 000 Polubień (rozłożone do 8 TikToków)",
+     "40 000 Wyświetleń (rozłożone do 4 TikToków)",
+     "Gwarancja 90 Dni"
+    ],
+    price: 1999
+   },
+   {
+    id: 14,
+    name: "Pakiet High",
+    list: [
+     "2000 Polskich obserwacji",
+     "16 000 Międzynarodowych obserwacji",
+     "25 000 Polubień (rozłożone do 5 TikToków)",
+     "80 000 Wyświetleń (rozłożone do 8 TikToków)",
+     "Gwarancja 90 Dni"
+    ],
+    price: 2999
+   },
+   {
+    id: 15,
+    name: "Pakiet Konto",
+    list: [
+     "Konto TikTok z ponad 15 000 obserwacji (90% polskich odbiorców)",
+     "2000 Polskich obserwacji",
+     "30 000 Międzynarodowych obserwacji",
+     "35 000 Polubień (rozłożone do 10 TikToków)",
+     "100 000 Wyświetleń do (rozłożone do 10 TikToków)",
+     "Gwarancja 90 Dni"
+    ],
+    price: 7299
+   },
+   {
+    id: 16,
+    name: "Pakiet Pro",
+    list: [
+     "Konto TikTok z ponad 50 000 obserwacji (90% polskich odbiorców)",
+     "60 000 Międzynarodowych obserwacji",
+     "75 000 Polubień (rozłożone do 10 TikToków)",
+     "500 000 Wyświetleń do (rozłożone do 25 TikToków)",
+     "Gwarancja 90 Dni"
+    ],
+    price: 13999
+   }
+  ],
+  others: [
+   {
+    id: 17,
+    name: "10 000 Wyświetleń",
+    image: "/service-images/tiktok/10000-views.png",
+    price: 149
+   },
+   {
+    id: 18,
+    name: "10 000 Międzynarodowych obserwacji",
+    image: "/service-images/tiktok/10000-follow.png",
+    price: 149
+   },
+   {
+    id: 19,
+    name: "1000 Międzynarodowych polubień",
+    image: "/service-images/tiktok/1000-like.png",
+    price: 119
+   },
+   {
+    id: 20,
+    name: "1000 Zapisów",
+    image: "/service-images/tiktok/1000-saves.png",
+    price: 99
+   },
+   {
+    id: 21,
+    name: "1000 Udostępnień",
+    image: "/service-images/tiktok/1000-shares.png",
+    price: 99
+   }
+  ]
+ }
+};
+
 const Services = () => {
- return <div></div>;
+ return (
+  <div className="w-full h-full p-4 pt-8">
+   <h1 className="w-full flex flex-col items-center text-5xl font-bold mb-8">
+    <FaInstagram className="w-24 h-24"/>
+    Usługi Instagram
+   </h1>
+   <h2 className="w-full text-center text-4xl font-bold mb-4 text-primary">
+    Pakiety
+   </h2>
+   <div className="flex flex-wrap justify-center gap-4 mb-8">
+    {services.instagram.packages.map((service) => (
+     <ServicePackageTile
+      key={service.id}
+      name={service.name}
+      list={service.list}
+      price={service.price}
+     />
+    ))}
+   </div>
+   <h2 className="w-full text-center text-4xl font-bold mb-4 text-primary">
+    Pozostałe
+   </h2>
+   <div className="flex flex-wrap justify-center gap-4 mb-8">
+    {services.instagram.others.map((service) => (
+     <ServiceOthersTile
+      key={service.id}
+      name={service.name}
+      image={service.image}
+      price={service.price}
+     />
+    ))}
+   </div>
+   <h1 className="w-full flex flex-col items-center text-5xl font-bold mb-8 gap-4">
+    <FaTiktok className="w-24 h-24"/>
+    Usługi TikTok
+   </h1>
+   <h2 className="w-full text-center text-4xl font-bold mb-4 text-primary">
+    Pakiety
+   </h2>
+   <div className="flex flex-wrap justify-center gap-4 mb-8">
+    {services.tiktok.packages.map((service) => (
+     <ServicePackageTile
+      key={service.id}
+      name={service.name}
+      list={service.list}
+      price={service.price}
+     />
+    ))}
+   </div>
+   <h2 className="w-full text-center text-4xl font-bold mb-4 text-primary">
+    Pozostałe
+   </h2>
+   <div className="flex flex-wrap justify-center gap-4">
+    {services.tiktok.others.map((service) => (
+     <ServiceOthersTile
+      key={service.id}
+      name={service.name}
+      image={service.image}
+      price={service.price}
+     />
+    ))}
+   </div>
+  </div>
+ );
 };
 
 export default Services;
