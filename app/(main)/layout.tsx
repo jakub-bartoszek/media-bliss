156789e36951b1d60868b32ff692e3926c2b8f96
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-14 19:43:10 +0200

    add: added opacity effect for navigation

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,47 @@
+"use client";
+
 import Footer from "@/components/footer";
 import Navigation from "@/components/navigation";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function RootLayout({
  children
 }: Readonly<{
  children: React.ReactNode;
 }>) {
+ const [opacity, setOpacity] = useState(0);
+
+ useEffect(() => {
+  const handleScroll = () => {
+   const scrollY = window.scrollY;
+   if (scrollY >= 300) {
+    setOpacity(100);
+   } else if (scrollY >= 200) {
+    setOpacity(66);
+   } else if (scrollY >= 100) {
+    setOpacity(33);
+   } else {
+    setOpacity(0);
+   }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
+  return () => {
+   window.removeEventListener("scroll", handleScroll);
+  };
+ }, []);
+
+ const headerClasses = `h-14 flex justify-center fixed top-0 left-0 w-full z-30 transition border-b-2 border-transparent ${
+  opacity === 100 && "bg-white shadow-[0_0_40px_rgba(0,0,0,0.2)]"
+ } ${opacity === 66 && "bg-white/60"} ${
+  opacity === 33 && "bg-white/30"
+ } ${opacity === 0 && "bg-white/0"}`;
+
  return (
   <>
-   <header className="h-14 flex justify-center bg-white fixed top-0 left-0 w-full shadow-[0_0_30px_#00000050] z-30">
+   <header className={headerClasses}>
     <Navigation />
    </header>
    <main className="max-w-[1400px] ml-auto mr-auto">
