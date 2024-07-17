d3b7d23a613f81f46888a3c6f40b84231c9d2b99
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-17 19:29:36 +0200

    add: added homepage to admin panel

---
+++ app/admin/(admin-panel)/home/page.tsx
@@ -0,0 +1,14 @@
+const AdminHomePage = () => {
+ return (
+  <div className="w-full h-full flex flex-col items-center justify-center">
+   <h1 className="text-white text-2xl md:text-4xl lg:text-6xl text-center whitespace-nowrap">
+    Mediabliss admin panel
+   </h1>
+   <div className="flex items-center justify-between text-white/40">
+    <h2>1.0.0</h2>
+   </div>
+  </div>
+ );
+};
+
+export default AdminHomePage;
