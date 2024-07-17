5fb33fec92b455a930087cad4ad8d706e9804b44
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-17 19:08:20 +0200

    change: renamed AdminLayout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,7 @@
-export default function AdminLayout({
+export default function AdminRootLayout({
  children
 }: Readonly<{
  children: React.ReactNode;
 }>) {
- return (
-  <div className="w-full h-full bg-black/90">
-   {children}
-  </div>
- );
+ return <div className="w-full h-full bg-black/90">{children}</div>;
 }
