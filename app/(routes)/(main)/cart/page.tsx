592c25e9866a62061698eb06b54c25cbf6e83733
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-21 14:55:17 +0200

    change: redesigned cart and success page

--- app/(routes)/(main)/cart/page.tsx
+++ app/(routes)/(main)/cart/page.tsx
@@ -68,57 +68,97 @@ const Cart = () => {
  };
 
  return (
-  <div className="w-full h-full p-4 pt-16">
-   <h1 className="text-3xl font-bold mb-4">Your Cart</h1>
-   {cartItems.length > 0 ? (
-    <div className="flex flex-col gap-4">
-     {cartItems.map((item) => (
-      <div
-       key={item.cartId}
-       className="flex justify-between items-center p-4 border rounded-lg shadow-md bg-white"
-      >
-       <div>
-        <h2 className="text-xl font-semibold">{item.name}</h2>
-        <p className="text-lg text-gray-500">
-         {Number(item.price).toFixed(2)} PLN
-        </p>
-       </div>
-       {item.type === "Service" || item.type === "CustomService" ? (
-        <input
-         className="border-2"
-         placeholder="Link do konta"
-         value={item.accountLink}
-         onChange={(e) =>
-          updateAccountLink(item.cartId, e.target.value)
-         }
-        />
-       ) : (
-        "Produkt zostanie dostarczony na email"
-       )}
-       <button
-        className="px-4 py-2 bg-red-500 text-white rounded-lg"
-        onClick={() => removeItemFromCart(item.cartId)}
-       >
-        Remove
-       </button>
+  <div className="w-full h-full p-4 pt-16 bg-gray-100 text-black flex flex-col justify-center">
+   <h1 className="text-3xl font-bold mb-8 text-gray-800 mt-8">
+    Twój koszyk
+   </h1>
+   <div className="w-full flex flex-col md:flex-row gap-4">
+    <div className="w-full md:w-2/3">
+     {cartItems.length > 0 ? (
+      <div className="flex flex-col gap-4">
+       {cartItems.map((item) => (
+        <div
+         key={item.cartId}
+         className="flex justify-between items-center p-4 border rounded-lg gap-4"
+        >
+         <div className="w-1/2">
+          <h2 className="text-xl font-semibold ">{item.name}</h2>
+          <p className="text-sm text-gray-500">{item.category}</p>
+          <span className="text-lg font-semibold mr-4 text-primary">
+           {Number(item.price).toFixed(2)} PLN
+          </span>
+         </div>
+         <div className="flex items-center w-1/2 justify-between">
+          {item.type === "Service" ||
+          item.type === "CustomService" ? (
+           <input
+            className="p-2 border border-gray-300 rounded-md text-gray-700 w-full"
+            placeholder="Link do konta"
+            value={item.accountLink}
+            onChange={(e) =>
+             updateAccountLink(item.cartId, e.target.value)
+            }
+           />
+          ) : (
+           <p className="text-gray-500">
+            Produkt zostanie dostarczony na email
+           </p>
+          )}
+          <button
+           className="ml-4 p-2 text-gray-500 hover:text-gray-700 transition"
+           onClick={() => removeItemFromCart(item.cartId)}
+          >
+           <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+           >
+            <path
+             strokeLinecap="round"
+             strokeLinejoin="round"
+             strokeWidth="2"
+             d="M6 18L18 6M6 6l12 12"
+            ></path>
+           </svg>
+          </button>
+         </div>
+        </div>
+       ))}
       </div>
-     ))}
-     <div className="flex justify-between items-center p-4 border-t mt-4">
-      <h2 className="text-xl font-semibold">Total</h2>
-      <p className="text-lg text-gray-500">
-       {calculateTotal().toFixed(2)} PLN
-      </p>
-     </div>
-     <button
-      className="mt-4 px-4 py-2 bg-green-500 text-white rounded-lg self-end"
-      onClick={handleBuy}
-     >
-      Buy
-     </button>
+     ) : (
+      <p className="text-lg text-gray-500">Twój koszyk jest pusty.</p>
+     )}
     </div>
-   ) : (
-    <p className="text-lg text-gray-500">Your cart is empty.</p>
-   )}
+    <div className="w-full md:w-1/3">
+     <div className="p-6 border rounded-lg bg-white">
+      <h2 className="text-2xl font-semibold">Podsumowanie</h2>
+      <div className="mt-4">
+       <div className="flex justify-between">
+        <span>Wartość produktów</span>
+        <span>{calculateTotal().toFixed(2)} PLN</span>
+       </div>
+       <div className="flex justify-between mt-2">
+        <span>Dostawa</span>
+        <span>0.00 PLN</span>
+       </div>
+      </div>
+      <div className="flex justify-between items-center mt-4 pt-4 border-t border-gray-300">
+       <span className="text-xl font-bold">Razem</span>
+       <span className="text-xl font-bold">
+        {calculateTotal().toFixed(2)} PLN
+       </span>
+      </div>
+      <button
+       className="mt-6 w-full px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-light transition"
+       onClick={handleBuy}
+      >
+       Zapłać
+      </button>
+     </div>
+    </div>
+   </div>
   </div>
  );
 };
--- app/(routes)/(main)/success/page.tsx
+++ app/(routes)/(main)/success/page.tsx
@@ -1,8 +1,34 @@
+"use client";
+
 const Success = () => {
  return (
-  <div>
-   <h1>Payment Successful</h1>
-   <p>Thank you for your purchase! Your order is being processed.</p>
+  <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800 p-4">
+   <div className="max-w-lg w-full p-6 bg-white shadow-md rounded-lg text-center">
+    <svg
+     className="w-16 h-16 mx-auto text-primary"
+     fill="none"
+     stroke="currentColor"
+     viewBox="0 0 24 24"
+     xmlns="http://www.w3.org/2000/svg"
+    >
+     <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M9 12l2 2l4 -4m6 2a9 9 0 1 1 -18 0a9 9 0 0 1 18 0z"
+     ></path>
+    </svg>
+    <h1 className="text-3xl font-bold mt-4">Płatność zakończona sukcesem</h1>
+    <p className="text-lg mt-2 text-gray-600">
+     Dziękujemy za zakup! Twoje zamówienie jest przetwarzane.
+    </p>
+    <button
+     className="mt-6 px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-light transition"
+     onClick={() => window.location.href = '/services/all'}
+    >
+     Kontynuuj zakupy
+    </button>
+   </div>
   </div>
  );
 };
