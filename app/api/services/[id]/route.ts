887b48535178141627782fecfa1ec5f3adb286d2
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-22 20:32:45 +0200

    add: added patch and delete routes

--- app/api/services/[id]/route.ts
+++ app/api/services/[id]/route.ts
@@ -37,8 +37,16 @@ export async function GET(
 
 export async function POST(request: NextRequest) {
  try {
-  const { name, price, description, list, image, category, type } =
-   await request.json();
+  const {
+   name,
+   price,
+   description,
+   list,
+   image,
+   category,
+   type,
+   requireLink
+  } = await request.json();
 
   const newService = await prisma.service.create({
    data: {
@@ -48,11 +56,69 @@ export async function POST(request: NextRequest) {
     list,
     image,
     category,
-    type
+    type,
+    requireLink
    }
   });
 
   return NextResponse.json(newService);
+ } catch (error) {
+  console.error(error);
+  return NextResponse.error();
+ }
+}
+
+export async function PATCH(
+ request: NextRequest,
+ { params }: { params: { id: string } }
+) {
+ const serviceId = parseInt(params.id);
+
+ try {
+  const {
+   name,
+   price,
+   description,
+   list,
+   image,
+   category,
+   type,
+   requireLink
+  } = await request.json();
+
+  const updatedService = await prisma.service.update({
+   where: { id: serviceId },
+   data: {
+    name,
+    price,
+    description,
+    list,
+    image,
+    category,
+    type,
+    requireLink
+   }
+  });
+
+  return NextResponse.json(updatedService);
+ } catch (error) {
+  console.error(error);
+  return NextResponse.error();
+ }
+}
+
+export async function DELETE(
+ request: NextRequest,
+ { params }: { params: { id: string } }
+) {
+ const serviceId = parseInt(params.id);
+
+ try {
+  const updatedService = await prisma.service.delete({
+   where: { id: serviceId }
+  });
+
+  return NextResponse.json(updatedService);
  } catch (error) {
   console.error(error);
   return NextResponse.error();
