1347ef4d4af344db5598e183b13fcd4f12cb84b5
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-24 23:27:10 +0200

    add: clearing localstorage after purchase

--- app/(routes)/(main)/success/page.tsx
+++ app/(routes)/(main)/success/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
+import { useEffect } from "react";
+
 const Success = () => {
+ useEffect(() => {
+  localStorage.removeItem("cart");
+ }, []);
+
  return (
   <div className="w-full h-screen flex flex-col items-center justify-center bg-zinc-100 text-zinc-800 p-4">
    <div className="max-w-lg w-full p-6 bg-white shadow-md rounded-lg text-center">
@@ -18,13 +24,15 @@ const Success = () => {
       d="M9 12l2 2l4 -4m6 2a9 9 0 1 1 -18 0a9 9 0 0 1 18 0z"
      ></path>
     </svg>
-    <h1 className="text-3xl font-bold mt-4">Płatność zakończona sukcesem</h1>
+    <h1 className="text-3xl font-bold mt-4">
+     Płatność zakończona sukcesem
+    </h1>
     <p className="text-lg mt-2 text-zinc-600">
      Dziękujemy za zakup! Twoje zamówienie jest przetwarzane.
     </p>
     <button
      className="mt-6 px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-light transition"
-     onClick={() => window.location.href = '/services/all'}
+     onClick={() => (window.location.href = "/services/all")}
     >
      Kontynuuj zakupy
     </button>
