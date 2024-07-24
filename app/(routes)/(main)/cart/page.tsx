19ebf1f801aa94ca9c1cea0f3f94b459f9974aff
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-24 14:31:00 +0200

    change: added accept terms checkbox

--- app/(routes)/(main)/cart/page.tsx
+++ app/(routes)/(main)/cart/page.tsx
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
 import { CartItemWithAccountLink } from "@/types";
+import { twMerge } from "tailwind-merge";
+import { FaCheck, FaCross, FaShoppingCart } from "react-icons/fa";
+import { RxCross1 } from "react-icons/rx";
+import { BiTrash } from "react-icons/bi";
 
 const stripePromise = loadStripe(
  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || ""
@@ -14,6 +18,7 @@ const Cart = () => {
   CartItemWithAccountLink[]
  >([]);
  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+ const [termsAccepted, setTermsAccepted] = useState(false);
 
  useEffect(() => {
   const storedCart = JSON.parse(localStorage.getItem("cart") || "[]");
@@ -111,107 +116,135 @@ const Cart = () => {
  };
 
  return (
-  <div className="w-full h-full p-4 pt-16 bg-zinc-100 text-black flex flex-col justify-center">
-   <h1 className="text-3xl font-bold mb-8 text-zinc-800 mt-8">
-    Twój koszyk
-   </h1>
-   <div className="w-full flex flex-col md:flex-row gap-4">
-    <div className="w-full md:w-2/3">
-     {cartItems.length > 0 ? (
-      <div className="flex flex-col gap-4">
-       {cartItems.map((item) => (
-        <div
-         key={item.cartId}
-         className="flex justify-between items-center p-4 border rounded-lg gap-4"
-        >
-         <div className="w-1/3">
-          <h2 className="text-xl font-semibold">{item.name}</h2>
-          <p className="text-sm text-zinc-500">{item.description}</p>
-          <span className="text-lg font-semibold mr-4 text-primary">
-           {Number(item.price).toFixed(2)} PLN
-          </span>
-          {item.image && (
-           <img
-            src={item.image}
-            alt={item.name}
-            className="w-16 h-16 object-cover"
-           />
-          )}
-         </div>
-         <div className="flex items-center w-2/3 justify-between">
-          <div className="w-full">
-           {item.requireLink === "true" ? (
-            <input
-             className="p-2 border border-zinc-300 rounded-md text-zinc-700 w-full"
-             placeholder="Link do konta"
-             value={item.accountLink}
-             onChange={(e) => {
-              updateAccountLink(item.cartId, e.target.value);
-              validateAccountLink(
-               item.cartId,
-               e.target.value,
-               item.category
-              );
-             }}
-            />
-           ) : (
-            <p className="text-sm text-center">
-             Produkt zostanie dostarczony na adres email
-            </p>
-           )}
-          </div>
-          <button
-           className="ml-4 p-2 text-zinc-500 hover:text-zinc-700 transition"
-           onClick={() => removeItemFromCart(item.cartId)}
-          >
-           <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-           >
-            <path
-             strokeLinecap="round"
-             strokeLinejoin="round"
-             strokeWidth="2"
-             d="M6 18L18 6M6 6l12 12"
-            ></path>
-           </svg>
-          </button>
-         </div>
-        </div>
-       ))}
-      </div>
-     ) : (
-      <p className="text-lg text-zinc-500">Twój koszyk jest pusty.</p>
-     )}
+  <div className="w-full h-full bg-zinc-100 text-black flex flex-col justify-center">
+   {cartItems.length === 0 ? (
+    <div className="w-full h-screen flex items-center justify-center flex-col text-zinc-400">
+     <div>
+      <FaShoppingCart className="w-24 h-24 text-zinc-500" />
+     </div>
+     <div>Brak produktów w koszyku</div>
     </div>
-    <div className="w-full md:w-1/3">
-     <div className="p-6 border rounded-lg bg-white">
-      <h2 className="text-2xl font-semibold">Podsumowanie</h2>
-      <div className="mt-4">
-       <div className="flex justify-between">
-        <span>Wartość produktów</span>
-        <span>{calculateTotal().toFixed(2)} PLN</span>
+   ) : (
+    <div className="p-4 pt-16">
+     <h1 className="text-3xl font-bold mb-8 text-zinc-800 mt-8">
+      Twój koszyk
+     </h1>
+     <div className="w-full flex flex-col md:flex-row gap-4">
+      <div className="w-full md:w-2/3">
+       <div className="flex flex-col gap-4">
+        {cartItems.map((item) => (
+         <div
+          key={item.cartId}
+          className="flex justify-between items-center p-4 border rounded-lg gap-4"
+         >
+          <div className="w-1/3">
+           <h2 className="text-xl font-semibold">{item.name}</h2>
+           <p className="text-sm text-zinc-500">{item.category}</p>
+           <p className="text-sm text-zinc-500">{item.description}</p>
+           <span className="text-lg font-semibold mr-4 text-primary">
+            {Number(item.price).toFixed(2)} PLN
+           </span>
+          </div>
+          <div className="flex items-center w-2/3 justify-between">
+           <div className="w-full">
+            {item.requireLink === "true" ? (
+             <div className="relative">
+              <input
+               className="p-2 border border-zinc-300 rounded-md text-zinc-700 w-full"
+               placeholder="Link do konta"
+               value={item.accountLink}
+               onChange={(e) => {
+                const newLink = e.target.value;
+                updateAccountLink(item.cartId, newLink);
+                validateAccountLink(
+                 item.cartId,
+                 newLink,
+                 item.category
+                );
+               }}
+              />
+              {errors[item.cartId] && (
+               <div className="absolute text-red-500 text-sm mt-1">
+                {errors[item.cartId]}
+               </div>
+              )}
+             </div>
+            ) : (
+             <p className="text-sm text-center">
+              Produkt zostanie dostarczony na adres email
+             </p>
+            )}
+           </div>
+           <button
+            className="ml-4 p-2 text-zinc-500 hover:text-zinc-700 transition"
+            onClick={() => removeItemFromCart(item.cartId)}
+           >
+            <BiTrash className="w-6 h-6" />
+           </button>
+          </div>
+         </div>
+        ))}
        </div>
       </div>
-      <div className="flex justify-between items-center mt-4 pt-4 border-t border-zinc-300">
-       <span className="text-xl font-bold">Razem</span>
-       <span className="text-xl font-bold">
-        {calculateTotal().toFixed(2)} PLN
-       </span>
+      <div className="w-full md:w-1/3">
+       <div className="p-6 border rounded-lg bg-white">
+        <h2 className="text-2xl font-semibold">Podsumowanie</h2>
+        <div className="mt-4">
+         <div className="flex justify-between">
+          <span>Wartość produktów</span>
+          <span>{calculateTotal().toFixed(2)} PLN</span>
+         </div>
+        </div>
+        <div className="flex justify-between items-center mt-4 pt-4 border-t border-zinc-300">
+         <span className="text-xl font-bold">Razem</span>
+         <span className="text-xl font-bold">
+          {calculateTotal().toFixed(2)} PLN
+         </span>
+        </div>
+        <div className="mt-6">
+         <div className="flex items-center space-x-3 mb-4">
+          <div
+           onClick={() => setTermsAccepted(!termsAccepted)}
+           className={twMerge(
+            "min-w-6 min-h-6 border-2 border-primary rounded-lg flex items-center justify-center transition cursor-pointer",
+            termsAccepted
+             ? "bg-primary"
+             : "hover:border-primary-light"
+           )}
+          >
+           <FaCheck
+            className={twMerge(
+             "text-white h-4 w-4 transition-transform",
+             termsAccepted
+              ? "transform scale-100"
+              : "transform scale-0"
+            )}
+           />
+          </div>
+          <span>
+           Zapoznałem/am się i akceptuję{" "}
+           <a
+            className="underline"
+            href="/terms-of-service"
+           >
+            regulamin
+           </a>{" "}
+           MediaBliss
+          </span>
+         </div>
+         <button
+          className="w-full px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-light transition disabled:bg-zinc-500"
+          onClick={handleBuy}
+          disabled={!termsAccepted}
+         >
+          Zapłać
+         </button>
+        </div>
+       </div>
       </div>
-      <button
-       className="mt-6 w-full px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-light transition"
-       onClick={handleBuy}
-       disabled={Object.keys(errors).length > 0}
-      >
-       Zapłać
-      </button>
      </div>
     </div>
-   </div>
+   )}
   </div>
  );
 };
--- app/(routes)/(main)/layout.tsx
+++ app/(routes)/(main)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
   <>
    <Header />
    <main className="max-w-[1400px] ml-auto mr-auto">
-    <div className="w-full min-h-screen h-full flex flex-col items-center">
+    <div className="w-full min-h-screen flex flex-col items-center">
      {children}
     </div>
    </main>
