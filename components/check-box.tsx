64d1e86dcb7a9d89e071fc828610e2a3da168992
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-25 18:52:35 +0200

    add: added checkbox component

---
+++ components/check-box.tsx
@@ -0,0 +1,31 @@
+import { FaCheck } from "react-icons/fa";
+import { twMerge } from "tailwind-merge";
+
+interface CheckBoxProps {
+ onClick: () => void;
+ className?: string;
+ checked?: boolean;
+}
+
+const CheckBox = ({ onClick, className, checked }: CheckBoxProps) => {
+ return (
+  <div
+   onClick={onClick}
+   className={twMerge(
+    "group w-min h-min p-1 border-2 border-primary rounded-lg flex items-center justify-center transition cursor-pointer",
+    checked &&
+     "bg-primary bg-gradient-to-bl from-white/20 hover:brightness-125",
+    className
+   )}
+  >
+   <FaCheck
+    className={twMerge(
+     "text-white h-3 w-3 transition group-hover:text-primary group-hover:scale-75 scale-0",
+     checked && "scale-100 group-hover:text-white group-hover:scale-100"
+    )}
+   />
+  </div>
+ );
+};
+
+export default CheckBox;
