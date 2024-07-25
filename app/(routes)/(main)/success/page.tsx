93fb468fab621771fc7e8057307cc626730d0680
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-25 18:53:23 +0200

    change: changed styles of success page

--- app/(routes)/(main)/success/page.tsx
+++ app/(routes)/(main)/success/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Button from "@/components/button";
 import { useEffect } from "react";
 
 const Success = () => {
@@ -8,8 +9,8 @@ const Success = () => {
  }, []);
 
  return (
-  <div className="w-full h-screen flex flex-col items-center justify-center bg-zinc-100 text-zinc-800 p-4">
-   <div className="max-w-lg w-full p-6 bg-white shadow-md rounded-lg text-center">
+  <div className="w-full h-screen flex flex-col items-center justify-center text-zinc-800 p-4">
+   <div className="max-w-lg w-full p-6 text-center">
     <svg
      className="w-16 h-16 mx-auto text-primary"
      fill="none"
@@ -24,18 +25,13 @@ const Success = () => {
       d="M9 12l2 2l4 -4m6 2a9 9 0 1 1 -18 0a9 9 0 0 1 18 0z"
      ></path>
     </svg>
-    <h1 className="text-3xl font-bold mt-4">
-     Płatność zakończona sukcesem
-    </h1>
+    <h1 className="text-3xl font-bold mt-4">Płatność zakończona sukcesem</h1>
     <p className="text-lg mt-2 text-zinc-600">
      Dziękujemy za zakup! Twoje zamówienie jest przetwarzane.
     </p>
-    <button
-     className="mt-6 px-4 py-2 bg-primary text-white rounded-lg hover:bg-primary-light transition"
-     onClick={() => (window.location.href = "/services/all")}
-    >
-     Kontynuuj zakupy
-    </button>
+    <a href="/services/all">
+     <Button className="mt-4">Kontynuuj zakupy</Button>
+    </a>
    </div>
   </div>
  );
