c2db92f4c56df7239d934a48e2d22c7349e694c2
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-29 13:56:06 +0200

    add: added patch and delete route

--- app/api/customers/[id]/route.ts
+++ app/api/customers/[id]/route.ts
@@ -8,10 +8,7 @@ export async function GET(
  const customerId = parseInt(params.id);
 
  if (isNaN(customerId)) {
-  return NextResponse.json(
-   { error: "Invalid customer ID" },
-   { status: 400 }
-  );
+  return NextResponse.json({ error: "Invalid customer ID" }, { status: 400 });
  }
 
  try {
@@ -21,13 +18,52 @@ export async function GET(
   });
 
   if (!customer) {
-   return NextResponse.json(
-    { error: "Customer not found" },
-    { status: 404 }
-   );
+   return NextResponse.json({ error: "Customer not found" }, { status: 404 });
   }
 
   return NextResponse.json(customer);
+ } catch (error) {
+  console.error(error);
+  return NextResponse.error();
+ }
+}
+
+export async function PATCH(
+ request: NextRequest,
+ { params }: { params: { id: string } }
+) {
+ const customerId = parseInt(params.id);
+
+ try {
+  const { email, orders } = await request.json();
+
+  const updatedCustomer = await prisma.customer.update({
+   where: { id: customerId },
+   data: {
+    email,
+    orders
+   }
+  });
+
+  return NextResponse.json(updatedCustomer);
+ } catch (error) {
+  console.error(error);
+  return NextResponse.error();
+ }
+}
+
+export async function DELETE(
+ request: NextRequest,
+ { params }: { params: { id: string } }
+) {
+ const customerId = parseInt(params.id);
+
+ try {
+  const updatedCustomer = await prisma.customer.delete({
+   where: { id: customerId }
+  });
+
+  return NextResponse.json(updatedCustomer);
  } catch (error) {
   console.error(error);
   return NextResponse.error();
