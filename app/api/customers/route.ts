e9c4c0cb416a005b21ad525cbd6ab01069648105
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-07-30 15:22:09 +0200

    fix

--- app/api/customers/route.ts
+++ app/api/customers/route.ts
@@ -2,14 +2,14 @@ import { prisma } from "@/lib/server/database/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
- const emailParam = new URL(request.url).searchParams.get("status");
+ const emailParam = new URL(request.url).searchParams.get("email");
  const email = emailParam;
 
  try {
-  const orders = await prisma.order.findMany({
+  const customers = await prisma.customer.findMany({
    where: email ? { email } : {}
   });
-  return NextResponse.json(orders);
+  return NextResponse.json(customers);
  } catch (error) {
   console.error(error);
   return NextResponse.error();
