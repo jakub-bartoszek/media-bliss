3a044adb847af70b75b72c0ab1c306530db7eba0
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-08-19 13:29:46 +0200

    change: changed height

--- components/error.tsx
+++ components/error.tsx
@@ -2,8 +2,8 @@ import { BiError } from "react-icons/bi";
 
 const Error = () => {
  return (
-  <div className="w-full h-[calc(100vh-56px)] flex flex-col items-center justify-center">
-   <BiError className="w-48 h-48 text-primary" />
+  <div className="w-full flex flex-col items-center justify-center">
+   <BiError className="w-48 h-48  " />
    <p className="text-3xl font-bold">Coś poszło nie tak...</p>
   </div>
  );
