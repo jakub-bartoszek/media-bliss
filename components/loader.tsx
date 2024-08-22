591e8ddd7ffc97f98700030b2b9db00d0dda1ed9
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-08-23 01:27:55 +0200

    change: changed loader height to screen

--- components/loader.tsx
+++ components/loader.tsx
@@ -2,7 +2,7 @@ import { LuLoader2 } from "react-icons/lu";
 
 const Loader = () => {
  return (
-  <div className="w-full flex items-center justify-center">
+  <div className="w-full h-screen flex items-center justify-center">
    <LuLoader2 className="animate-spin w-16 h-16 text-accent" />
   </div>
  );
