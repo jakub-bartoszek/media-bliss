88cb68a0a7890172e1fa7e2e32cf62b319e693d8
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-09-01 15:56:37 +0200

    change: working disabled styles

--- components/button.tsx
+++ components/button.tsx
@@ -21,7 +21,7 @@ const Button = ({
    disabled={disabled}
    type={type}
    className={twMerge(
-    "bg-accent bg-gradient-to-bl from-white/20 rounded-lg py-2 px-4 w-max text-white transition whitespace-nowrap hover:brightness-125 disabled:bg-zinc-600 disabled:hover:brightness-100",
+    "w-max whitespace-nowrap rounded-lg bg-accent bg-gradient-to-bl from-white/20 px-4 py-2 text-white transition hover:brightness-125 disabled:bg-zinc-600 disabled:bg-none disabled:hover:brightness-100",
     className
    )}
    onClick={onClick}
