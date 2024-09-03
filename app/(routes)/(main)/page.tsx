d12bf1f25b7e801154dabea07b03d22ba2084966
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-09-03 16:47:19 +0200

    add: added icons to home sections

--- app/(routes)/(main)/page.tsx
+++ app/(routes)/(main)/page.tsx
@@ -1,38 +1,50 @@
 import { Banner } from "@/components/banner";
 import HomeSection from "@/components/home-section";
+import { IoMegaphone } from "react-icons/io5";
+import { FaLightbulb, FaMedal, FaWallet } from "react-icons/fa";
+import { HiPuzzlePiece } from "react-icons/hi2";
+import { FaUserShield } from "react-icons/fa6";
+import { RiHandHeartFill } from "react-icons/ri";
 
 const sections = [
  {
+  icon: <IoMegaphone className="h-20 w-20 -rotate-12" />,
   header: "Zwiększanie popularności",
   description:
    "Specjalizujemy się w budowaniu zasięgów na platformach społecznościowych, co pozwala dotrzeć do szerokiej grupy odbiorców."
  },
  {
+  icon: <FaLightbulb className="h-20 w-20" />,
   header: "Nowatorskie rozwiązania",
   description:
    "Każda usługa jest indywidualnie dostosowywana do potrzeb klienta, co zwiększa skuteczność działań promocyjnych."
  },
  {
+  icon: <HiPuzzlePiece className="h-20 w-20" />,
   header: "Dostosowane usługi",
   description:
    "Każda usługa jest indywidualnie dostosowywana do potrzeb klienta, co zwiększa skuteczność działań promocyjnych."
  },
  {
+  icon: <FaMedal className="h-20 w-20" />,
   header: "Doświadczenie i profesjonalizm",
   description:
    "Nasz zespół z wieloletnim doświadczeniem zapewnia wysoką jakość usług."
  },
  {
+  icon: <FaUserShield className="h-20 w-20" />,
   header: "Budowanie wizerunku",
   description:
    "Pomagamy w kreowaniu i zarządzaniu wizerunkiem marki, co zwiększa zaufanie i wartość na rynku."
  },
  {
+  icon: <RiHandHeartFill className="h-20 w-20" />,
   header: "Indywidualne podejście",
   description:
    "Traktujemy każdego klienta indywidualnie, dostosowując strategie do jego celów."
  },
  {
+  icon: <FaWallet className="h-20 w-20" />,
   header: "Efektywne zarządzanie budżetem",
   description:
    "Nasze doświadczenie pozwala na maksymalizację zwrotu z inwestycji przy efektywnym zarządzaniu budżetami reklamowymi."
@@ -49,6 +61,7 @@ export default function Home() {
       key={index}
       header={section.header}
       description={section.description}
+      icon={section.icon}
      />
     ))}
    </div>
--- components/home-section.tsx
+++ components/home-section.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { useRef } from "react";
+import { ReactNode, useRef } from "react";
 import { twMerge } from "tailwind-merge";
 import { useIsVisible } from "@/lib/hooks/useIsVisible";
 
 interface HomeSectionProps {
  header: string;
  description: string;
+ icon?: ReactNode;
 }
 
-const HomeSection = ({ header, description }: HomeSectionProps) => {
+const HomeSection = ({ header, description, icon }: HomeSectionProps) => {
  const sectionRef = useRef(null);
  const isVisible = useIsVisible(sectionRef);
 
@@ -21,6 +22,7 @@ const HomeSection = ({ header, description }: HomeSectionProps) => {
     isVisible && "opacity-100"
    )}
   >
+   <div className="w-full flex items-center justify-center p-4">{icon}</div>
    <h1 className="text-4xl font-bold mb-8">{header}</h1>
    <p className="text-sm text-muted sm:text-base md:text-lg">{description}</p>
   </div>
